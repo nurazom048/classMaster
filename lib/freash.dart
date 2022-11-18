0700ef0341160f489802691cd863cb110e12d0e9
nurazom050 <nurazom048@gmail.com>
2022-11-19 00:17:32 +0600

    appbar

--- lib/freash.dart
+++ lib/freash.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:table/classdetals.dart';
 import 'package:table/main.dart';
@@ -20,145 +22,191 @@ class MyWidget extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(
+        body: Column(
+          children: [
+            CustomTopBar("Kpi 7/1/ET-C", ontap: () {}),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    TopContaner(
-                        priode: 'Priode',
-                        startTime: "sevendays",
-                        endtime: "Time"),
-                    Wrap(
-                      direction: Axis.vertical,
-                      children: List.generate(
-                        sevendays.length,
-                        (index) => Container(
-                          decoration: BoxDecoration(
-                              color: index % 2 == 0
-                                  ? const Color.fromRGBO(207, 213, 234, 1)
-                                  : Colors.black12,
-                              border: const Border(
-                                  right: BorderSide(
-                                      color: Colors.black, width: 1))),
-                          height: 100,
-                          width: 100,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(sevendays[index]),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ]),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Wrap(
-                    direction: Axis.horizontal,
-                    children: List.generate(
-                      7,
-                      (index) => TopContaner(
-                        priode: "${index + 1}",
-                        startTime: "8:00",
-                        endtime: "8:45",
-                      ),
-                    ),
-                  ),
-
-                  ////////////////////////////// class data
-
-                  Wrap(
-                    direction: Axis.vertical,
-                    children: List.generate(
-                      classdata.length,
-                      ((classindex) => Wrap(
-                            direction: Axis.horizontal,
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          TopContaner(
+                              priode: 'Priode',
+                              startTime: "sevendays",
+                              endtime: "Time"),
+                          Wrap(
+                            direction: Axis.vertical,
                             children: List.generate(
-                              classdata[classindex].date.length,
-                              (index) => InkWell(
-                                onTap: classdata[classindex]
-                                            .date[index]
-                                            .subjectcode
-                                            .toString() ==
-                                        "00"
-                                    ? null
-                                    : () => Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  Classdetails(
-                                                      classdate:
-                                                          classdata[classindex]
-                                                              .date[index])),
-                                        ),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: classindex % 2 == 0
-                                          ? const Color.fromRGBO(
-                                              207, 213, 234, 1)
-                                          : Colors.black12,
-                                      border: const Border(
-                                          right: BorderSide(
-                                              color: Colors.black, width: 1))),
-                                  height: 100,
-                                  width: (((classdata[classindex]
-                                                  .date[index]
-                                                  .endingpriode) -
-                                              (classdata[classindex]
-                                                  .date[index]
-                                                  .startingpriode)) >
-                                          0
-                                      ? 100 *
-                                          ((classdata[classindex]
-                                                      .date[index]
-                                                      .endingpriode -
-                                                  classdata[classindex]
-                                                      .date[index]
-                                                      .startingpriode) +
-                                              1)
-                                      : 100),
-                                  child: classdata[classindex]
-                                              .date[index]
-                                              .subjectcode
-                                              .toString() ==
-                                          "00"
-                                      ? const Center(child: Icon(Icons.clear))
-                                      : Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(classdata[classindex]
-                                                .date[index]
-                                                .instructorname),
-                                            Text(classdata[classindex]
-                                                .date[index]
-                                                .subjectcode),
-                                            Text(classdata[classindex]
-                                                .date[index]
-                                                .roomnum),
-                                          ],
-                                        ),
+                              sevendays.length,
+                              (index) => Container(
+                                decoration: BoxDecoration(
+                                    color: index % 2 == 0
+                                        ? const Color.fromRGBO(207, 213, 234, 1)
+                                        : Colors.black12,
+                                    border: const Border(
+                                        right: BorderSide(
+                                            color: Colors.black, width: 1))),
+                                height: 100,
+                                width: 100,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(sevendays[index]),
+                                  ],
                                 ),
                               ),
                             ),
-                          )),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
+                          ),
+                        ]),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Wrap(
+                          direction: Axis.horizontal,
+                          children: List.generate(
+                            7,
+                            (index) => TopContaner(
+                              priode: "${index + 1}",
+                              startTime: "8:00",
+                              endtime: "8:45",
+                            ),
+                          ),
+                        ),
+
+                        ////////////////////////////// class data
+
+                        Wrap(
+                          direction: Axis.vertical,
+                          children: List.generate(
+                            classdata.length,
+                            ((classindex) => Wrap(
+                                  direction: Axis.horizontal,
+                                  children: List.generate(
+                                    classdata[classindex].date.length,
+                                    (index) => InkWell(
+                                      onTap: classdata[classindex]
+                                                  .date[index]
+                                                  .subjectcode
+                                                  .toString() ==
+                                              "00"
+                                          ? null
+                                          : () => Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        Classdetails(
+                                                            classdate: classdata[
+                                                                    classindex]
+                                                                .date[index])),
+                                              ),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            color: classindex % 2 == 0
+                                                ? const Color.fromRGBO(
+                                                    207, 213, 234, 1)
+                                                : Colors.black12,
+                                            border: const Border(
+                                                right: BorderSide(
+                                                    color: Colors.black,
+                                                    width: 1))),
+                                        height: 100,
+                                        width: (((classdata[classindex]
+                                                        .date[index]
+                                                        .endingpriode) -
+                                                    (classdata[classindex]
+                                                        .date[index]
+                                                        .startingpriode)) >
+                                                0
+                                            ? 100 *
+                                                ((classdata[classindex]
+                                                            .date[index]
+                                                            .endingpriode -
+                                                        classdata[classindex]
+                                                            .date[index]
+                                                            .startingpriode) +
+                                                    1)
+                                            : 100),
+                                        child: classdata[classindex]
+                                                    .date[index]
+                                                    .subjectcode
+                                                    .toString() ==
+                                                "00"
+                                            ? const Center(
+                                                child: Icon(Icons.clear))
+                                            : Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
+                                                  Text(classdata[classindex]
+                                                      .date[index]
+                                                      .instructorname),
+                                                  Text(classdata[classindex]
+                                                      .date[index]
+                                                      .subjectcode),
+                                                  Text(classdata[classindex]
+                                                      .date[index]
+                                                      .roomnum),
+                                                  // Text((((classdata[classindex]
+                                                  //                 .date[index]
+                                                  //                 .endingpriode) -
+                                                  //             (classdata[classindex]
+                                                  //                 .date[index]
+                                                  //                 .startingpriode)) +
+                                                  //         1)
+                                                  //     .toString()),
+                                                ],
+                                              ),
+                                      ),
+                                    ),
+                                  ),
+                                )),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class CustomTopBar extends StatelessWidget {
+  String title;
+
+  // ignore: prefer_typing_uninitialized_variables
+  var ontap;
+  CustomTopBar(
+    this.title, {
+    required this.ontap,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(9.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                onPressed: () => Navigator.pop(context)),
+            Text(title),
+            IconButton(icon: const Icon(Icons.edit), onPressed: ontap),
+          ],
+        ));
   }
 }
